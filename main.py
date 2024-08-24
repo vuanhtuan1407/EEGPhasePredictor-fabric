319a0f99bf438de3a65319adcba482dc71419795
Tuan Vu <vuanhtuan1407@gmail.com>
2024-08-25 01:09:19 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import torch
+from src.eegpp import params
+from src.eegpp.trainer import EEGKFoldTrainer
+from src.eegpp.utils.model_utils import get_model
 
-a = torch.ones((2,5))
-b = torch.ones((2,5))
-t = torch.concat([a, b], dim=0)
-print(t.shape)
-
+if __name__ == "__main__":
+    model = get_model(params.MODEL_TYPE)
+    trainer = EEGKFoldTrainer(
+        model=model,
+        lr=params.LEARNING_RATE,
+        n_epochs=params.NUM_EPOCHS,
+        n_splits=params.N_SPLITS,
+        accelerator=params.ACCELERATOR,
+        devices=params.DEVICES,
+    )
+    trainer.fit()
